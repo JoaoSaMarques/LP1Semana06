0d9167e8743ae3ed16fd0bc0a186fbf78f0c0f4c
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-19 13:06:35 +0100

    Commented

--- GameSix/Enemy.cs
+++ GameSix/Enemy.cs
@@ -2,6 +2,7 @@
 
 namespace GameSix
 {
+    //Enemy maker
     public class Enemy
     {
         private string name;
@@ -17,29 +18,42 @@ namespace GameSix
             shield = 0;
         }
 
+        //Name checker
         public string GetName()
         {
+            //Tells you the name
             return name;
         }
 
+        //Health checker
         public string GetHealth()
         {
+            //Tells you the health amount
             return health;
         }
 
+        //Shield checker
         public string GetShield()
         {
+            //Tells you the shield amount
             return shield;
         }
 
+        //When taking damage
         public void TakeDamage(float damage)
         {
+            //Shield reduces damage first
             shield -= damage;
+            //If shield is 0
             if (shield < 0)
             {
+                //Whatever damage is left after shield is destroyed
                 float damageStillToInflict = -shield;
+                //make sure shield is at 0
                 shield = 0;
+                //Health takes leftover damage
                 health -= damageStillToInflict;
+                //If health is below 0 then have it be 0
                 if (health < 0) health = 0;
             }
         }
