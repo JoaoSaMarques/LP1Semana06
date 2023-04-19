956de33ff10a48e7c265405a352e3b69b457431f
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-19 18:42:42 +0100

    Every i enter a name/commented

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -8,12 +8,17 @@ namespace GameSix
         {
             //Ask number of enemies
             Console.WriteLine("Enter the number of enemies in the game: ");
+            //Convert string to int
             int EnemyNum = int.Parse(Console.ReadLine());
 
+            //Enemy number list
             Enemy[] enemies = new Enemy[EnemyNum];
 
+            //For i
             for (int i = 0; i < EnemyNum; i++)
             {
+                //Every i enter a name and then +1 until it reaches EnemyNum
+                Console.Write($"Enter the name of enemy #{i+1}: ");
 
             }
         }
