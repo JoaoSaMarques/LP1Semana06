f8ae0ab93321607f30d15a20c3e54b34692bc241
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-04-10 11:28:41 +0100

    put color and sphere into different cs

---
+++ ColorSpheres/Color.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.Intrinsics.X86;
+
+namespace ColorSpheres
+{
+    public class Color
+    {
+        public int Red { get; set; }
+        public int Blue { get; set; }
+        public int Green { get; set; }
+
+        public Color(int red, int blue, int green, int alpha)
+        {
+            Red = red;
+            Blue = blue;
+            Green = green;
+            alpha = 255;
+        }
+
+        public int GetGrey()
+        {
+            return (Red + Green + Blue) / 3;
+        }
+    }
+}
--- ColorSpheres/Sphere.cs
+++ ColorSpheres/Sphere.cs
@@ -4,26 +4,6 @@ using System.Runtime.Intrinsics.X86;
 
 namespace ColorSpheres
 {
-    public class Color
-    {
-        public int Red { get; set; }
-        public int Blue { get; set; }
-        public int Green { get; set; }
-
-        public Color(int red, int blue, int green, int alpha)
-        {
-            Red = red;
-            Blue = blue;
-            Green = green;
-            alpha = 255;
-        }
-
-        public int GetGrey()
-        {
-            return (Red + Green + Blue) / 3;
-        }
-    }
-
     public class Sphere 
     {
         public int cor { get; set; }
