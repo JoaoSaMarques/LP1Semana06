8ad96946abb642524f487bd5a29def0ae6865b27
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-04-10 11:10:47 +0100

    red blue and green and state that accepts all three

---
+++ ColorSpheres/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ColorSpheres
+{
+    public class Color
+    {
+        public int Red { get; set; }
+        public int Blue { get; set; }
+        public int Green { get; set; }
+
+        public Color(int red, int blue, int green)
+        {
+            Red = red;
+            Blue = blue;
+            Green = green;
+        }
+    }
+}
