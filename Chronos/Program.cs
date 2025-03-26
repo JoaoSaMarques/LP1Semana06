35f02f0eb2ae6dbdb276f3f9364d8e96ed8fdda3
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-03-26 12:07:31 +0000

    copy stopwatch example

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
-namespace Chronos
+class Chronos
 {
-    public class Program
+    static void Main(string[] args)
     {
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("Hello LP1!");
-        }
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        Thread.Sleep(10000);
+        stopWatch.Stop();
+        // Get the elapsed time as a TimeSpan value.
+        TimeSpan ts = stopWatch.Elapsed;
+
+        // Format and display the TimeSpan value.
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+        Console.WriteLine("RunTime " + elapsedTime);
     }
 }
