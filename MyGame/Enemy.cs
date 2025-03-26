e73196202b47b2eb28ebd8a747133bb1e523b714
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-03-26 12:29:37 +0000

    add TakeDamage part2

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -24,6 +24,13 @@ namespace MyGame
         public void TakeDamage(float damage)
         {
             shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
         }
     }
 }
